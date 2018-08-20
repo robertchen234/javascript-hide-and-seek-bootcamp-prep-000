8cffdeabaf81d96970447bb4d2e0234dcd5b2c6b
Robert Chen <robertchen234@gmail.com>
2018-08-20 03:31:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  var deep = document.getElementById('grand-node')
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
 }
